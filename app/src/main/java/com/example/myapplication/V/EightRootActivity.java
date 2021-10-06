d30d71cdca6b98160a367f8a6a700c6e3a8bb8ba
Negar Andishgan <parsaprhadi>
2021-10-06 09:37:05 +0330

    twe

--- app/src/main/java/com/example/myapplication/V/EightRootActivity.java
+++ app/src/main/java/com/example/myapplication/V/EightRootActivity.java
@@ -26,6 +26,10 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.myapplication.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -41,7 +45,7 @@ public class EightRootActivity extends AppCompatActivity {
 
     int text_play=0;
     int notchcount;
-
+    GraphView graphView1,graphView2,graphView3,graphView4,graphView5,graphView6,graphView7,graphView8;
     int playcount;
     @Override
     public void onBackPressed() {
@@ -205,6 +209,215 @@ public class EightRootActivity extends AppCompatActivity {
         });
         DrawerLayout drawerLayout=findViewById(R.id.draver_eightroot);
         btn.setOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
+    /////////////////////////////////////////////////////////////////////////////////////////////
+        graphView1 = findViewById(R.id.eightgraphview1);
+
+        LineGraphSeries<DataPoint> series1 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+        //// graphView.setTitleTextSize(9);
+
+        graphView1.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graphView1.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graphView1.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.NONE);
+        graphView1.addSeries(series1);
+
+        series1.setColor(Color.rgb(30,30,30));
+
+        series1.setDrawBackground(false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+        graphView2 = findViewById(R.id.eightgraphview2);
+
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+        //// graphView.setTitleTextSize(9);
+
+        graphView2.addSeries(series2);
+
+        series2.setColor(Color.rgb(30,30,30));
+
+        graphView2.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graphView2.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graphView2.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.NONE);
+        series2.setDrawBackground(false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+        graphView3 = findViewById(R.id.eightgraphview3);
+
+        LineGraphSeries<DataPoint> series3 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+        //// graphView.setTitleTextSize(9);
+
+        graphView3.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graphView3.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graphView3.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.NONE);
+        graphView3.addSeries(series3);
+
+        series3.setColor(Color.rgb(30,30,30));
+
+        series3.setDrawBackground(false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+        graphView4 = findViewById(R.id.eightgraphview4);
+
+        LineGraphSeries<DataPoint> series4 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+        //// graphView.setTitleTextSize(9);
+
+        graphView4.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graphView4.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graphView4.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.NONE);
+        graphView4.addSeries(series4);
+
+        series4.setColor(Color.rgb(30,30,30));
+
+        series4.setDrawBackground(false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+        graphView5 = findViewById(R.id.eightgraphview5);
+
+        LineGraphSeries<DataPoint> series5 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+        //// graphView.setTitleTextSize(9);
+
+        graphView5.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graphView5.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graphView5.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.NONE);
+        graphView5.addSeries(series5);
+
+        series5.setColor(Color.rgb(30,30,30));
+
+        series5.setDrawBackground(false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+        graphView6 = findViewById(R.id.eightgraphview6);
+
+        LineGraphSeries<DataPoint> series6 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+        //// graphView.setTitleTextSize(9);
+
+        graphView6.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graphView6.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graphView6.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.NONE);
+        graphView6.addSeries(series6);
+
+        series6.setColor(Color.rgb(30,30,30));
+
+        series6.setDrawBackground(false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+        graphView7 = findViewById(R.id.eightgraphview7);
+
+        LineGraphSeries<DataPoint> series7 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+        //// graphView.setTitleTextSize(9);
+
+        graphView7.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graphView7.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graphView7.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.NONE);
+        graphView7.addSeries(series7);
+
+        series7.setColor(Color.rgb(30,30,30));
+
+        series7.setDrawBackground(false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+        graphView8 = findViewById(R.id.eightgraphview8);
+
+        LineGraphSeries<DataPoint> series8 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+
+
+        graphView8.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graphView8.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.HORIZONTAL);
+        graphView8.addSeries(series8);
+
+        series8.setColor(Color.rgb(30,30,30));
+        series8.setDrawBackground(false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+
     }
 
     private void FindViewById() {
--- app/src/main/java/com/example/myapplication/V/SingleRootActivity.java
+++ app/src/main/java/com/example/myapplication/V/SingleRootActivity.java
@@ -27,6 +27,10 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.myapplication.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -40,7 +44,7 @@ public class SingleRootActivity extends AppCompatActivity {
     Dialog dialog;
     TextView textplay;
     ImageView notch;
-
+    GraphView graphView;
     Button line,btn,montage,bluetooth,play;
 
     int notchcount;
@@ -208,6 +212,35 @@ public class SingleRootActivity extends AppCompatActivity {
         });
         DrawerLayout drawerLayout=findViewById(R.id.draver_singleroot);
         btn.setOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
+/////////////////////////////////////////////////////////////////////////////////////////////
+        graphView = findViewById(R.id.singlegraphview);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 9),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6),
+                new DataPoint(7, 1),
+                new DataPoint(8, 2)
+        });
+        graphView.setTitle("My Graph View");
+       //// graphView.setTitleTextSize(9);
+
+        graphView.addSeries(series);
+
+        series.setColor(Color.rgb(30,30,30));
+
+        series.setDrawBackground(false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
     }
 
     private void FindViewById() {
